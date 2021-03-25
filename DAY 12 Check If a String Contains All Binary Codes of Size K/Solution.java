dadf74fcfc34ef459df0b33b53d42e2144a36ef8
Rohit Mittal <43905130+rohitm131@users.noreply.github.com>
2021-03-25 20:14:43 +0530

    Create Solution.java

---
+++ DAY 12 Check If a String Contains All Binary Codes of Size K/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        
+        //BruteForce approach
+        Set<Integer> set= new HashSet<>();
+        for(int i=0;i<=s.length()-k;i++)
+        { 
+                set.add(Integer.parseInt(s.substring(i,i+k),2));// Converting into decimal and then adding it to set.
+        }
+        for(int i=0;i<Math.pow(2,k);i++)// checking if all elements are present or not
+        {
+            if(!set.contains(i))
+                return false;
+        }
+        return true;
+
+        
+    }
+}
